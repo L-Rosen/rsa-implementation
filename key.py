ee4e901f0fdd8eaf0e1f6472c19d52ae8efd5731
Lucas Rosenzweig <xeit0w@gmail.com>
2025-03-20 02:51:23 +0100

    Implement encrypt/decrypt args + fixed messageToBlocks and BlocksToMessage

--- key.py
+++ key.py
@@ -148,25 +148,22 @@ def blockSizeCalc(n,alphabet):
 def letterToNumber(alphabet, i, letter): 
       return replaceAlphabet(alphabet,letter)*(len(alphabet)**i)
 
-def messageToBlocks(message,alphabet,block_size):
+def messageToBlocks(message,alphabet,block_size,autofillChar):
 
-      blocks = []
-      somme = 0
-      i = block_size
-      for letter in message:
-            i = i-1
-            somme += letterToNumber(alphabet, i, letter)
-            if i == 0:
-                  blocks.append(somme)
-                  i = block_size
-                  somme = 0
-
-      if i > 0:
-            for j in range(i):
-                  i -= 1
-                  somme+= letterToNumber(alphabet, i, ' ')
-            blocks.append(somme)
-      return blocks
+        blocks = []
+        i = 0
+        while i < len(message):
+                block = 0
+                letter = ""
+                for j in range(block_size,0,-1):
+                    if i < len(message):
+                            letter += message[i]
+                            block += letterToNumber(alphabet,j-1,message[i])
+                            i += 1
+                    else:
+                            block += letterToNumber(alphabet,j-1,autofillChar)
+                blocks.append(block)
+        return blocks
 
 def blocksToMessage(blocks, alphabet, block_size):
       message = ""
@@ -366,6 +363,13 @@ if __name__ == "__main__":
 
       args = parser.parse_args()
 
+      alphabet = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, 
+                        "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22,
+                        "x": 23, "y": 24, "z": 25, "A": 26, "B": 27, "C": 28, "D": 29, "E": 30, "F": 31, "G": 32, "H": 33,
+                        "I": 34, "J": 35, "K": 36, "L": 37, "M": 38, "N": 39, "O": 40, "P": 41, "Q": 42, "R": 43, "S": 44,
+                        "T": 45, "U": 46, "V": 47, "W": 48, "X": 49, "Y": 50, "Z": 51, "_": 52, ".": 53, ",": 54, ";": 55,
+                        ":": 56, "!": 57, "?": 58, "'": 59}
+
       if args.mode == "keygen":
             e,d,n,phi = keygen(args.keysize)
             print(f"Taille de la clé : {args.keysize}")
@@ -374,114 +378,50 @@ if __name__ == "__main__":
             print(f"Module n : {n}")
       
       elif args.mode == "encrypt":
-
-            alphabet = {
-                        "a": 0,
-                        "b": 1,
-                        "c": 2,
-                        "d": 3,
-                        "e": 4,
-                        "f": 5,
-                        "g": 6,
-                        "h": 7,
-                        "i": 8,
-                        "j": 9,
-                        "k": 10,
-                        "l": 11,
-                        "m": 12,
-                        "n": 13,
-                        "o": 14,
-                        "p": 15,
-                        "q": 16,
-                        "r": 17,
-                        "s": 18,
-                        "t": 19,
-                        "u": 20,
-                        "v": 21,
-                        "w": 22,
-                        "x": 23,
-                        "y": 24,
-                        "z": 25,
-                        "A": 26,
-                        "B": 27,
-                        "C": 28,
-                        "D": 29,
-                        "E": 30,
-                        "F": 31,
-                        "G": 32,
-                        "H": 33,
-                        "I": 34,
-                        "J": 35,
-                        "K": 36,
-                        "L": 37,
-                        "M": 38,
-                        "N": 39,
-                        "O": 40,
-                        "P": 41,
-                        "Q": 42,
-                        "R": 43,
-                        "S": 44,
-                        "T": 45,
-                        "U": 46,
-                        "V": 47,
-                        "W": 48,
-                        "X": 49,
-                        "Y": 50,
-                        "Z": 51,
-                        " ": 52,
-                        ".": 53,
-                        ",": 54,
-                        ";": 55,
-                        ":": 56,
-                        "!": 57,
-                        "?": 58,
-                        "'": 59
-                  }
-            
-            if not (args.key or not args.n) and (args.messageint or args.messagetxt):
+            if not args.key or not args.n:
                   e,d,n,phi = keygen(args.keysize)
                   print(f"Aucune clé fournie , génération de clé aléatoire")
-                  print(f"Clé publique : ({args.key},{args.n})")
-                  print(f"Clé privée : ({d},{args.n})")
+                  print(f"Clé publique : ({e},{n})")
+                  print(f"Clé privée : ({d},{n})")
             else:
                   e,n = args.key,args.n
 
-            if not args.messageint and not args.messagetxt:
+            if not args.messageint and not args.messagetxt: #Si aucun message n'est spécifié
                   print("Vous devez spécifier un message à chiffrer")
             
-            elif args.messageint and args.messagetxt:
+            elif args.messageint and args.messagetxt: #Si les deux types de messages sont spécifiés
                   print("Vous devez spécifier un seul type de message")
 
-            elif args.messageint and not args.messagetxt:
-                  encrypted = encrypt(args.message,e,n)
-                  print(f"Message chiffré: {args.message}")
+            elif args.messageint and not args.messagetxt: #Si le message est un nombre
+                  encrypted = encrypt(args.messageint,e,n)
+                  print(f"Message chiffré: {encrypted}")
 
-            elif args.messagetxt and not args.messageint:
-                  print(e,n)
+            elif args.messagetxt and not args.messageint: #Si le message est un texte
                   blockSize = blockSizeCalc(n,alphabet)
-                  blocks = messageToBlocks(args.message,alphabet,blockSize)
+                  blocks = messageToBlocks(args.messagetxt,alphabet,blockSize,"_")
                   encrypted = encryptBlocks(blocks,e,n)
-                  print(f"Message chiffré : {blocksToMessage(encrypted,alphabet,blockSize)}")
+                  print(f"Message chiffré : {blocksToMessage(encrypted,alphabet,blockSize+1)}")
                   
             
       elif args.mode == "decrypt":
-            if not args.key or not args.n or not args.message:
+            if not args.key or not args.n or (not args.messageint and not args.messagetxt):
                   print("Vous devez spécifier l'exposant de déchiffrement, le module et le message à déchiffrer")
-            else:
-                  print(decrypt(args.message,args.key,args.n))
 
-      elif args.mode == "encryption-test":
-            if not args.message:
-                  print("Vous devez spécifier un message")
-            else:
-                  e,d,n,phi = keygen(32)
-                  print(f"Taille de la clé : {args.keysize}")
-                  print(f"Exposant publique : {e}")
-                  print(f"Exposant privé : {d}")
-                  print(f"Module n : {n}")
-                  print(f"Message : {args.message}")
-                  print(f"Message chiffré : {encrypt(args.message,e,n)}")
-                  print(f"Message déchiffré : {decrypt(encrypt(args.message,e,n),d,n)}")
+            elif not args.messageint and not args.messagetxt: #Si aucun message n'est spécifié
+                  print("Vous devez spécifier un message à déchiffrer")
+            
+            elif args.messageint and args.messagetxt: #Si les deux types de messages sont spécifiés
+                  print("Vous devez spécifier un seul type de message")
+
+            elif args.messageint and not args.messagetxt: #Si le message est un nombre
+                  encrypted = encrypt(args.messageint,args.key,args.n)
+                  print(f"Message déchiffré: {encrypted}")
+
+            elif args.messagetxt and not args.messageint: #Si le message est un texte
+                  blockSize = blockSizeCalc(args.n,alphabet)+1
+                  blocks = messageToBlocks(args.messagetxt,alphabet,blockSize,"_")
+                  decrypted = decryptBlocks(blocks,args.key,args.n)
+                  print(f"Message déchiffré : {blocksToMessage(decrypted,alphabet,blockSize-1)}")
       
       elif args.mode == "factorize":
            if not args.n or not args.key or not args:
