bccde033a51a68d6fbead978e475a17e36dfad75
Lucas Rosenzweig <xeit0w@gmail.com>
2025-03-20 03:53:54 +0100

    Implement sign and verify

--- key.py
+++ key.py
@@ -69,69 +69,6 @@ def factorize(n):
             if isPrime(p) and isPrime(q) and n%p == 0:
                   return p,q
 
-"""
-Implémentation
-
-e,d,n,phi = keygen(110)
-m = random.randint(2,phi-1)
-c = encrypt(m,e,n)
-m1 = decrypt(c,d,n)
-colorPrint(GREEN,f"Message : {m}")
-colorPrint(YELLOW,f"Message chiffré : {c}")
-colorPrint(GREEN, f"Message déchiffré : {m1}")
-"""
-
-"""
-Test avec une clé de taille petite
-
-e = 12413
-p,q = factorize(13289)
-n = p*q
-phi = phiCalc(p,q)
-r, d = extendedEuclid(e, phi)
-m = [9197, 6284, 12836, 8709, 4584, 10239, 11553, 4584, 7008, 12523,
-     9862, 356, 5356, 1159, 10280, 12523, 7506, 6311]
-
-colorPrint(GREEN,f"Message non chiffré :\n")
-for message in m:
-      print(message,end=" ; ")
-
-print()
-colorPrint(YELLOW,f"Message déchiffré :\n")
-for message in m:
-      print(decrypt(message,d,n),end=" ; ")
-
-print()
-colorPrint(GREEN,f"Message re-chiffré :\n")
-for message in m:
-      print(encrypt(decrypt(message,d,n),e,n),end=" ; ")
-
-La complexité est de n*sqrt(n)
-"""
-
-"""
-e = 163119273
-p,q = factorize(755918011)
-n = p*q
-phi = phiCalc(p,q)
-r, d = extendedEuclid(e, phi)
-m = [671828605, 407505023, 288441355, 679172842, 180261802]
-
-colorPrint(GREEN,f"Message non chiffré :\n")
-for message in m:
-      print(message,end=" ; ")
-
-print()
-colorPrint(YELLOW,f"Message déchiffré :\n")
-for message in m:
-      print(decrypt(message,d,n),end=" ; ")
-
-print()
-colorPrint(GREEN,f"Message re-chiffré :\n")
-for message in m:
-      print(encrypt(decrypt(message,d,n),e,n),end=" ; ")
-"""
-
 def replaceAlphabet(alphabet,letter): return alphabet[letter]
 
 def replaceNumber(alphabet, number): 
@@ -187,179 +124,31 @@ def decryptBlocks(blocks,d,n):
             blocksBis.append(decrypt(c,d,n))
       return blocksBis
 
-"""
-RSA ET CODAGE
-alphabet = {
-      "a": 0,
-      "b": 1,
-      "c": 2,
-      "d": 3,
-      "e": 4,
-      "f": 5,
-      "g": 6,
-      "h": 7,
-      "i": 8,
-      "j": 9,
-      "k": 10,
-      "l": 11,
-      "m": 12,
-      "n": 13,
-      "o": 14,
-      "p": 15,
-      "q": 16,
-      "r": 17,
-      "s": 18,
-      "t": 19,
-      "u": 20,
-      "v": 21,
-      "w": 22,
-      "x": 23,
-      "y": 24,
-      "z": 25,
-      "A": 26,
-      "B": 27,
-      "C": 28,
-      "D": 29,
-      "E": 30,
-      "F": 31,
-      "G": 32,
-      "H": 33,
-      "I": 34,
-      "J": 35,
-      "K": 36,
-      "L": 37,
-      "M": 38,
-      "N": 39,
-      "O": 40,
-      "P": 41,
-      "Q": 42,
-      "R": 43,
-      "S": 44,
-      "T": 45,
-      "U": 46,
-      "V": 47,
-      "W": 48,
-      "X": 49,
-      "Y": 50,
-      "Z": 51,
-      " ": 52,
-      ".": 53,
-      ",": 54,
-      ";": 55,
-      ":": 56,
-      "!": 57,
-      "?": 58,
-      "'": 59
-}
-
-print(YELLOW,end="")
-e,d,n,phi = keygen(int(input("Taille de la clé : ")))
-
-print(RESET,end="")
-colorPrint(CYAN,f"Clé publique : {e,n}")
-colorPrint(CYAN,f"Clé privée : {d,n}")
-colorPrint(CYAN,f"Phi : {phi}\n")
-print(YELLOW,end="")
-
-message = input("Message a chiffrer: ")
-print(RESET,end="")
-
-blockSize = blockSizeCalc(n,alphabet)
-colorPrint(CYAN,f"Taille des blocs : {blockSize}\n")
-
-blocks = messageToBlocks(message,alphabet,blockSize)
-colorPrint(GREEN,f"Message : {message}")
-colorPrint(GREEN,f"Message en blocs : {blocks}\n")
-
-blocks = encryptBlocks(blocks,e,n)
-blocks_bis = blocksToMessage(blocks,alphabet,blockSize+1)
-colorPrint(RED,f"Message chiffré : {blocks_bis}")
-colorPrint(RED,f"Message chiffré en blocks : {blocks}\n")
-
-blocks = decryptBlocks(blocks,d,n)
-colorPrint(GREEN,f"Message déchiffré en blocs: {blocks}")
-colorPrint(GREEN,f"Message déchiffré : {blocksToMessage(blocks,alphabet,blockSize)}")"
-"""
-
 def hashFile(file):
       with open(file,"rb") as f:
-            return sha256(f.read()).hexdigest()
+            content = f.read()
+            h = sha256(content).hexdigest()
+            hint = int(h,16)
+            return hint
 
-def signHash(hash,d,n):
-      alphabet_hex = {
-            "0": 0,
-            "1": 1,
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9,
-            "a": 10,
-            "b": 11,
-            "c": 12,
-            "d": 13,
-            "e": 14,
-            "f": 15,
-            " ": 16
-      }
+def sign(file,d,n):
+      h = hashFile(file) % n
+      return encrypt(h,d,n)
 
-      block_size = blockSizeCalc(n,alphabet_hex)
-      blocks = messageToBlocks(hash,alphabet_hex,block_size)
-      encrypt_blocks = encryptBlocks(blocks,d,n)
-      return encrypt_blocks
-
-def verifyHash(hash,signature,e,n):
-      alphabet_hex = {
-            "0": 0,
-            "1": 1,
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9,
-            "a": 10,
-            "b": 11,
-            "c": 12,
-            "d": 13,
-            "e": 14,
-            "f": 15,
-            " ": 16
-      }
-
-      block_size = blockSizeCalc(n,alphabet_hex)
-      blocks = decryptBlocks(signature,e,n)
-      message = blocksToMessage(blocks,alphabet_hex,block_size).replace(" ","")
-      return message == hash
-
-"""
-e,d,n,phi = keygen(32)
-hash = hashFile("input.txt")
-signature = signHash(hash,d,n)
-verifyHash(hash,signature,e,n)
-
-colorPrint(CYAN,f"Hash : {hash}")
-colorPrint(CYAN,f"Signature : {signature}")
-
-if verifyHash(hash,signature,e,n):
-      colorPrint(GREEN,"Signature valide")
-else:
-      colorPrint(RED,"Signature invalide")
-"""
+def verify(file,signature,e,n):
+      h = hashFile(file) % n
+      return h == decrypt(signature,e,n)
 
 if __name__ == "__main__":
       parser = argparse.ArgumentParser(description="Outil RSA en ligne de commande")
-      parser.add_argument("mode", choices=["keygen", "encrypt", "decrypt","encryption-test","factorize"], help="Action à effectuer")
+      parser.add_argument("mode", choices=["keygen", "encrypt", "decrypt","factorize","sign","verify"], help="Action à effectuer")
       parser.add_argument("--keysize", type=int, help="Taille de la clé en bits", default=32)
       parser.add_argument("--key", type=int, help="Exposant pour chiffrer/déchiffrer")
       parser.add_argument("--n", type=int, help="Module n")
       parser.add_argument("--messageint", type=int, help="Message (nombre) à chiffrer/déchiffrer")
       parser.add_argument("--messagetxt", type=str, help="Message (texte) à chiffrer/déchiffrer")
+      parser.add_argument("--file", type=str, help="Fichier à signer")
+      parser.add_argument("--signature", type=str, help="Signature à vérifier")
 
       args = parser.parse_args()
 
@@ -442,3 +231,27 @@ if __name__ == "__main__":
                               print("Erreur lors de l'attaque par factorisation")
                         else:
                               print(f"Message déchiffré : {decrypt}")
+
+      elif args.mode == "sign":
+        if not args.file:
+            print("Vous devez spécifier un fichier à signer")
+        else:
+            e,d,n,phi = None,args.key,args.n, None
+            if not args.key or not args.n:
+                e,d,n,phi = keygen(args.keysize)
+                print(f"Aucune clé fournie, génération de clé aléatoire")
+                print(f"Clé publique : ({e},{n})")
+                print(f"Clé privée : ({d},{n})")
+
+            signature = sign(args.file, d, n)
+            print(f"Signature : {signature}")
+
+      elif args.mode == "verify":
+        if not args.file or not args.key or not args.n or not args.signature:
+            print("Vous devez spécifier un fichier, une clé publique, le module n et la signature à vérifier")
+        else:
+            is_valid = verify(args.file, int(args.signature), args.key, args.n)
+            if is_valid:
+                print("Signature valide")
+            else:
+                print("Signature invalide")
